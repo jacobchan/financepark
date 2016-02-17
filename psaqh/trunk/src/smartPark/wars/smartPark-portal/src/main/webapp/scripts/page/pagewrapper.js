394f02aeac8421404c82f233d50ed7e3dd2bcde7
zhulizhi <zhulizhi@cd981f65-b658-4eb0-b96c-9f6a8eb296b4>
2016-02-17 10:17:49 +0000

    提交门户网站内容

---
+++ psaqh/trunk/src/smartPark/wars/smartPark-portal/src/main/webapp/scripts/page/pagewrapper.js
@@ -0,0 +1,26 @@
+/*!
+ * giui JavaScript Library v3.0.0
+ * 
+ *
+ * Copyright 2016-2020, zhyi_12
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ * Date: 2016-2-16
+ */
+$(function(){
+	var _passportBaseUrl = 'http://192.168.80.1:8088/gsoft-web/';
+	
+	$('#youi_page_header').load($.youi.serverConfig.contextPath+'/common/header.html',function(){
+		$.getScript(_passportBaseUrl+'portal/userInfo.html',function(){
+			var loc = encodeURIComponent(window.location.href);
+			if($.youi.serverConfig.authorization){
+				console.log('user authorization = '+$.youi.serverConfig.authorization);
+				$('#user_info:first').append('<a href="'+_passportBaseUrl+'portal/logout.html?redirect='+loc+'">退出</a>');
+			}else{
+				$('#user_info:first a:first').attr('href',_passportBaseUrl+'member/portal/login.html?redirect='+loc);
+			}
+		});
+	});
+	
+	$('#youi_page_footer').load($.youi.serverConfig.contextPath+'/common/footer.html');
+});
