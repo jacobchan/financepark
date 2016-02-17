1e82213d6ba445b86c60d78df73f3781f9c252ba
zhulizhi <zhulizhi@cd981f65-b658-4eb0-b96c-9f6a8eb296b4>
2016-02-17 10:18:59 +0000

    提交门户网站内容

---
+++ psaqh/trunk/src/smartPark/wars/smartPark-portal/src/main/webapp/scripts/giui/giui.plist.js
@@ -0,0 +1,61 @@
+/*!
+ * youi JavaScript Library v3.0.0
+ * 
+ *
+ * Copyright 2016-2020, zhyi_12
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ * Date: 2016-1-4
+ */
+
+require("./giui.core.js");
+
+(function($) {
+//	var _log = $.youi.log;
+	/**
+	 *  page组件
+	 * @author  zhouyi
+	 * @version 1.0.0
+	 */
+	
+	$.widget("youi.plist",$.youi.abstractWidget,$.extend({},{
+		options:{
+			bindResize:true
+		},
+		
+		_initWidget:function(){
+			//jsonp
+			var that = this;
+			$.youi.ajaxUtils.ajax({
+				//url:'http://192.168.80.1:8088/gsoft-web/esb/web/userManager/getPagerUsers.json',
+				//url:'http://localhost:9088/jsonp/data.jsp?data:file=users.json',
+				url:this.options.jsonpSrc,
+				jsonp:'data:jsonp',
+				dataType:'jsonp',
+				success:function(results){
+					if(results&&results.records){
+						that._parseRecords(results.records);
+					}
+				}
+			});
+		},
+		
+		_parseRecords:function(records){
+			var htmls = [];
+			
+			var template = '<div class="list-item">'+this.options.template+'</div>';
+			
+			$(records).each(function(){
+				htmls.push($.youi.recordUtils.replaceByRecord(template,this));
+			});
+			
+			this.element.html(htmls.join(''));
+		},
+		
+		_destroy:function(){
+			//调用页面销毁函数
+			this._callGloablFunc('destroy');
+		}
+	}));
+	
+})(jQuery);
