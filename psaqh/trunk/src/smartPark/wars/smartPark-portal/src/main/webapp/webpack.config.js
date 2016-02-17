8eedf6ba4607583a5126f95388d51974c9538e84
zhulizhi <zhulizhi@cd981f65-b658-4eb0-b96c-9f6a8eb296b4>
2016-02-17 10:15:38 +0000

    提交门户网站

---
+++ psaqh/trunk/src/smartPark/wars/smartPark-portal/src/main/webapp/webpack.config.js
@@ -0,0 +1,17 @@
+﻿module.exports = {
+    entry: {
+    	'giui':["./scripts/giui/giui.resource.js",
+    	        "./scripts/giui/giui.plist.js"]
+    },
+    output: {
+        path: __dirname+'/scripts/lib',
+        filename: "[name].min.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/,exclude: /(node_modules|bower_components)/, loader: "style!css" },
+            { test: /\.jsx?$/,exclude: /(node_modules|bower_components)/,loader: 'jsx-loader?harmony'},
+            { test: /\.less$/, exclude: /(node_modules|bower_components)/,loader: "style!css!less" }
+        ]
+    }
+};
