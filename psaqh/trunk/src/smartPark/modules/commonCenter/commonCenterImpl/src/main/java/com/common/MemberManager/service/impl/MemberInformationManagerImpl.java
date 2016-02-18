7dab3e3769448d982f83b5b8671aa2c682940e9c
liuxj <liuxj@cd981f65-b658-4eb0-b96c-9f6a8eb296b4>
2016-02-18 06:51:58 +0000

    添加注册接口

--- psaqh/trunk/src/smartPark/modules/commonCenter/commonCenterImpl/src/main/java/com/common/MemberManager/service/impl/MemberInformationManagerImpl.java
+++ psaqh/trunk/src/smartPark/modules/commonCenter/commonCenterImpl/src/main/java/com/common/MemberManager/service/impl/MemberInformationManagerImpl.java
@@ -10,17 +10,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.gsoft.framework.security.agt.entity.User;
+import com.gsoft.framework.security.agt.service.UserManager;
 import com.gsoft.framework.core.exception.BusException;
 import com.gsoft.framework.core.orm.Condition;
 //import com.gsoft.framework.core.orm.ConditionFactory;
 import com.gsoft.framework.core.orm.Order;
 import com.gsoft.framework.core.orm.Pager;
 import com.gsoft.framework.core.orm.PagerRecords;
-
 import com.gsoft.framework.esb.annotation.*;
-
+import com.gsoft.framework.util.PasswordUtils;
 import com.gsoft.framework.core.service.impl.BaseManagerImpl;
-
 import com.common.MemberManager.entity.MemberInformation;
 import com.common.MemberManager.dao.MemberInformationDao;
 import com.common.MemberManager.service.MemberInformationManager;
@@ -30,6 +30,8 @@ import com.common.MemberManager.service.MemberInformationManager;
 public class MemberInformationManagerImpl extends BaseManagerImpl implements MemberInformationManager{
 	@Autowired
 	private MemberInformationDao memberInformationDao;
+	@Autowired
+	private UserManager userManager;
 	
     /**
      * 查询列表
@@ -101,6 +103,28 @@ public class MemberInformationManagerImpl extends BaseManagerImpl implements Mem
     
     public boolean exsitMemberInformation(String propertyName,Object value) throws BusException{
 		return memberInformationDao.exists(propertyName,value);
+	}
+    @EsbServiceMapping
+	public void saveReister(@ServiceParam(name="userName") String userName,
+			@ServiceParam(name="passwd") String passwd,
+			@ServiceParam(name="repasswd") String repasswd,
+			@ServiceParam(name="mobile") String mobile)
+			throws BusException {
+		MemberInformation memberInformationed = memberInformationDao.getObjectByUniqueProperty("memberName", userName);
+		if(memberInformationed==null){
+			User user = new User();
+			MemberInformation memberInformation = new MemberInformation();
+			memberInformation.setMemberName(userName);
+			memberInformation.setMemberDescribe2(PasswordUtils.md5Password(repasswd));
+			memberInformation.setMemberPhoneNumber(mobile);
+			user.setLoginName(userName);
+			user.setPassword(PasswordUtils.md5Password(repasswd));
+			user.setGroup("001");
+			userManager.saveUser(user);
+			memberInformationDao.save(memberInformation);
+	}else{
+		throw new BusException("该用户已存在!");
+		}
 	}
 
 }
