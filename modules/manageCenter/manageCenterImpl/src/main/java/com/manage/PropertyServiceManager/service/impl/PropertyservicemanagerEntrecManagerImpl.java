c19ea3ebabd8a8959d0db9ac8786d0f697f9d080
panxj <panxj@cd981f65-b658-4eb0-b96c-9f6a8eb296b4>
2016-02-18 09:57:51 +0000

    招商预约管理后台功能添加

--- modules/manageCenter/manageCenterImpl/src/main/java/com/manage/PropertyServiceManager/service/impl/PropertyservicemanagerEntrecManagerImpl.java
+++ modules/manageCenter/manageCenterImpl/src/main/java/com/manage/PropertyServiceManager/service/impl/PropertyservicemanagerEntrecManagerImpl.java
@@ -3,26 +3,29 @@
  */
 package com.manage.PropertyServiceManager.service.impl;
 
-import java.util.List;
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.common.MemberManager.dao.MemberInformationDao;
 import com.gsoft.framework.core.exception.BusException;
 import com.gsoft.framework.core.orm.Condition;
 //import com.gsoft.framework.core.orm.ConditionFactory;
 import com.gsoft.framework.core.orm.Order;
 import com.gsoft.framework.core.orm.Pager;
 import com.gsoft.framework.core.orm.PagerRecords;
-
-import com.gsoft.framework.esb.annotation.*;
-
 import com.gsoft.framework.core.service.impl.BaseManagerImpl;
-
-import com.manage.PropertyServiceManager.entity.PropertyservicemanagerEntrec;
+import com.gsoft.framework.esb.annotation.ConditionCollection;
+import com.gsoft.framework.esb.annotation.EsbServiceMapping;
+import com.gsoft.framework.esb.annotation.OrderCollection;
+import com.gsoft.framework.esb.annotation.ServiceParam;
+import com.manage.PropertyServiceManager.dao.PropertyservicemanagerEnteringDao;
 import com.manage.PropertyServiceManager.dao.PropertyservicemanagerEntrecDao;
+import com.manage.PropertyServiceManager.entity.PropertyservicemanagerEntering;
+import com.manage.PropertyServiceManager.entity.PropertyservicemanagerEntrec;
 import com.manage.PropertyServiceManager.service.PropertyservicemanagerEntrecManager;
 
 @Service("propertyservicemanagerEntrecManager")
@@ -31,6 +34,11 @@ public class PropertyservicemanagerEntrecManagerImpl extends BaseManagerImpl imp
 	@Autowired
 	private PropertyservicemanagerEntrecDao propertyservicemanagerEntrecDao;
 	
+	@Autowired
+	private MemberInformationDao memberInformationDao;
+	
+	@Autowired
+	private PropertyservicemanagerEnteringDao propertyservicemanagerEnteringDao;
     /**
      * 查询列表
      */
@@ -76,6 +84,39 @@ public class PropertyservicemanagerEntrecManagerImpl extends BaseManagerImpl imp
 //    	}
     	return propertyservicemanagerEntrecDao.save(o);
     }
+    
+    
+	/**
+	 * 前台用户入驻申请,生成入驻服务记录表，并修改预约数量
+	 * */
+	@EsbServiceMapping
+	public void enterApplication(PropertyservicemanagerEntrec propertyservicemanagerEntrec) throws BusException {
+//		AccountPrincipal account = SecurityUtils.getAccount();
+//		String userId = account.getLoginName();
+//		MemberInformation memberInformation=null;
+//		if(userId !=null){
+//			memberInformation=memberInformationDao.getObjectByUniqueProperty("memberNicknames", userId);
+//		}
+//		if(memberInformation != null){
+//			propertyservicemanagerEntrec.setMemberId(memberInformation.getMemberId());
+//		}
+		PropertyservicemanagerEntering enteringBefore=propertyservicemanagerEnteringDao.get(propertyservicemanagerEntrec.getPropertyservicemanagerEntering().getEnteringId());
+		PropertyservicemanagerEntrec entrec=new PropertyservicemanagerEntrec();
+		entrec.setPropertyservicemanagerEntering(enteringBefore);
+		entrec=propertyservicemanagerEntrecDao.save(propertyservicemanagerEntrec);//生成入驻服务预约表
+		if(entrec!=null){
+			String enteringId=entrec.getPropertyservicemanagerEntering().getEnteringId();
+			PropertyservicemanagerEntering entering=propertyservicemanagerEnteringDao.get(enteringId);
+			entering.setEnteringRemain(String.valueOf(Integer.valueOf(entering.getEnteringRemain())-1));//剩余预约数量
+			entering.setEnteringAlre(String.valueOf(Integer.valueOf(entering.getEnteringAlre())+1));
+			if(entering != null && entering.getEnteringRemain().equals("1")){//判断可办理预约表中剩余预约数量是否还有值
+				entering.setEnteringStatus("02");//剩余数量为0，修改预约数量状态为预约已满
+			}
+			propertyservicemanagerEnteringDao.save(entering);
+		}
+		
+		
+	}
 
     /**
      * 删除对象
