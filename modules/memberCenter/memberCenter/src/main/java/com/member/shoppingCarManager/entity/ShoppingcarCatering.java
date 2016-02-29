f0c63ff4d5a53818300b83b19bea6fec11236ae3
panxj <panxj@cd981f65-b658-4eb0-b96c-9f6a8eb296b4>
2016-02-29 06:57:58 +0000

    实体更新

--- modules/memberCenter/memberCenter/src/main/java/com/member/shoppingCarManager/entity/ShoppingcarCatering.java
+++ modules/memberCenter/memberCenter/src/main/java/com/member/shoppingCarManager/entity/ShoppingcarCatering.java
@@ -4,12 +4,10 @@
 package com.member.shoppingCarManager.entity;
 
 import javax.persistence.*;
-
 import org.hibernate.validator.*;
 
 import org.hibernate.annotations.GenericGenerator;
 
-import com.common.MemberManager.entity.MemberInformation;
 import com.gsoft.framework.core.dataobj.Domain;
 /**
  * 实体: -餐饮购物车
@@ -21,37 +19,65 @@ import com.gsoft.framework.core.dataobj.Domain;
 @Table(name = "sp_shoppingCar_catering")
 public class ShoppingcarCatering implements Domain{
 	
-	private static final long serialVersionUID = -7976477440708813581L;
+	private static final long serialVersionUID = 180614955197918113L;
 	
 
-	@Column(name = "COMPANY_CATERING_AMOUNT_")
-	@Length(max=16)
-	private String companyCateringAmount;//餐饮数量
-/*	@Id @GeneratedValue(generator="system-uuid")
-	@GenericGenerator(name="system-uuid", strategy = "uuid.hex")*/
+	@Column(name = "CREATE_TIME_")
+	private String createTime;//创建时间
+
+	@Column(name = "UPDATE_TIME_")
+	private String updateTime;//修改时间
+
+	@Column(name = "UPDATE_USER_")
+	@Length(max=36)
+	private String updateUser;//修改人
+	@Id @GeneratedValue(generator="system-uuid")
+	@GenericGenerator(name="system-uuid", strategy = "uuid.hex")
 	@Column(name = "COMMODITY_ID_")
 	@Length(max=36)
 	private String commodityId;//商品ID
-
-	@Column(name = "COMPANY_CATERING_UNIVALENCE_")
-	@Length(max=16)
-	private String companyCateringUnivalence;//餐饮单价
 	@Id @GeneratedValue(generator="system-uuid")
 	@GenericGenerator(name="system-uuid", strategy = "uuid.hex")
 	@Column(name = "COMPANY_CATERING_ID_")
 	@Length(max=36)
 	private String companyCateringId;//餐饮购物车ID
 
-	@ManyToOne(fetch=FetchType.EAGER)
-	@JoinColumn(name="MEMBER_ID_")
-	private MemberInformation memberId;//会员用户ID
+	@Column(name = "COMPANY_CATERING_AMOUNT_")
+	@Length(max=16)
+	private String companyCateringAmount;//餐饮数量
+
+	@Column(name = "CREATE_USER_")
+	@Length(max=36)
+	private String createUser;//创建人
+
+	@Column(name = "MEMBER_ID_")
+	@Length(max=36)
+	private String memberId;//会员用户ID
+
+	@Column(name = "COMPANY_CATERING_UNIVALENCE_")
+	@Length(max=16)
+	private String companyCateringUnivalence;//餐饮单价
 	
-	public String getCompanyCateringAmount(){
-		return this.companyCateringAmount;
+	public String getCreateTime(){
+		return this.createTime;
 	}
 	
-	public void setCompanyCateringAmount(String companyCateringAmount){
-		this.companyCateringAmount = companyCateringAmount;
+	public void setCreateTime(String createTime){
+		this.createTime = createTime;
+	}
+	public String getUpdateTime(){
+		return this.updateTime;
+	}
+	
+	public void setUpdateTime(String updateTime){
+		this.updateTime = updateTime;
+	}
+	public String getUpdateUser(){
+		return this.updateUser;
+	}
+	
+	public void setUpdateUser(String updateUser){
+		this.updateUser = updateUser;
 	}
 	public String getCommodityId(){
 		return this.commodityId;
@@ -60,13 +86,6 @@ public class ShoppingcarCatering implements Domain{
 	public void setCommodityId(String commodityId){
 		this.commodityId = commodityId;
 	}
-	public String getCompanyCateringUnivalence(){
-		return this.companyCateringUnivalence;
-	}
-	
-	public void setCompanyCateringUnivalence(String companyCateringUnivalence){
-		this.companyCateringUnivalence = companyCateringUnivalence;
-	}
 	public String getCompanyCateringId(){
 		return this.companyCateringId;
 	}
@@ -74,27 +93,50 @@ public class ShoppingcarCatering implements Domain{
 	public void setCompanyCateringId(String companyCateringId){
 		this.companyCateringId = companyCateringId;
 	}
-	
-	
-	
-	
-	public MemberInformation getMemberId() {
-		return memberId;
+	public String getCompanyCateringAmount(){
+		return this.companyCateringAmount;
 	}
-
-	public void setMemberId(MemberInformation memberId) {
+	
+	public void setCompanyCateringAmount(String companyCateringAmount){
+		this.companyCateringAmount = companyCateringAmount;
+	}
+	public String getCreateUser(){
+		return this.createUser;
+	}
+	
+	public void setCreateUser(String createUser){
+		this.createUser = createUser;
+	}
+	public String getMemberId(){
+		return this.memberId;
+	}
+	
+	public void setMemberId(String memberId){
 		this.memberId = memberId;
 	}
-
+	public String getCompanyCateringUnivalence(){
+		return this.companyCateringUnivalence;
+	}
+	
+	public void setCompanyCateringUnivalence(String companyCateringUnivalence){
+		this.companyCateringUnivalence = companyCateringUnivalence;
+	}
+	
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((companyCateringAmount == null) ? 0 : companyCateringAmount.hashCode());
+		result = prime * result + ((createTime == null) ? 0 : createTime.hashCode());
+		result = prime * result + ((updateTime == null) ? 0 : updateTime.hashCode());
+		result = prime * result + ((updateUser == null) ? 0 : updateUser.hashCode());
 		result = prime * result + ((commodityId == null) ? 0 : commodityId.hashCode());
-		result = prime * result + ((companyCateringUnivalence == null) ? 0 : companyCateringUnivalence.hashCode());
 		result = prime * result + ((companyCateringId == null) ? 0 : companyCateringId.hashCode());
+		result = prime * result + ((companyCateringAmount == null) ? 0 : companyCateringAmount.hashCode());
+		result = prime * result + ((createUser == null) ? 0 : createUser.hashCode());
 		result = prime * result + ((memberId == null) ? 0 : memberId.hashCode());
+		result = prime * result + ((companyCateringUnivalence == null) ? 0 : companyCateringUnivalence.hashCode());
 		return result;
 	}
 	
@@ -107,30 +149,50 @@ public class ShoppingcarCatering implements Domain{
 		if (getClass() != obj.getClass())
 			return false;
 		final ShoppingcarCatering other = (ShoppingcarCatering) obj;
-		if (companyCateringAmount == null) {
-			if (other.companyCateringAmount != null)
+		if (createTime == null) {
+			if (other.createTime != null)
 				return false;
-		} else if (!companyCateringAmount.equals(other.companyCateringAmount))
+		} else if (!createTime.equals(other.createTime))
+			return false;
+		if (updateTime == null) {
+			if (other.updateTime != null)
+				return false;
+		} else if (!updateTime.equals(other.updateTime))
+			return false;
+		if (updateUser == null) {
+			if (other.updateUser != null)
+				return false;
+		} else if (!updateUser.equals(other.updateUser))
 			return false;
 		if (commodityId == null) {
 			if (other.commodityId != null)
 				return false;
 		} else if (!commodityId.equals(other.commodityId))
 			return false;
-		if (companyCateringUnivalence == null) {
-			if (other.companyCateringUnivalence != null)
-				return false;
-		} else if (!companyCateringUnivalence.equals(other.companyCateringUnivalence))
-			return false;
 		if (companyCateringId == null) {
 			if (other.companyCateringId != null)
 				return false;
 		} else if (!companyCateringId.equals(other.companyCateringId))
 			return false;
+		if (companyCateringAmount == null) {
+			if (other.companyCateringAmount != null)
+				return false;
+		} else if (!companyCateringAmount.equals(other.companyCateringAmount))
+			return false;
+		if (createUser == null) {
+			if (other.createUser != null)
+				return false;
+		} else if (!createUser.equals(other.createUser))
+			return false;
 		if (memberId == null) {
 			if (other.memberId != null)
 				return false;
 		} else if (!memberId.equals(other.memberId))
+			return false;
+		if (companyCateringUnivalence == null) {
+			if (other.companyCateringUnivalence != null)
+				return false;
+		} else if (!companyCateringUnivalence.equals(other.companyCateringUnivalence))
 			return false;
 		return true;
 	}
--- modules/memberCenter/memberCenter/src/main/java/com/member/shoppingCarManager/entity/ShoppingcarCompanyserver.java
+++ modules/memberCenter/memberCenter/src/main/java/com/member/shoppingCarManager/entity/ShoppingcarCompanyserver.java
@@ -4,12 +4,10 @@
 package com.member.shoppingCarManager.entity;
 
 import javax.persistence.*;
-
 import org.hibernate.validator.*;
 
 import org.hibernate.annotations.GenericGenerator;
 
-import com.common.MemberManager.entity.MemberInformation;
 import com.gsoft.framework.core.dataobj.Domain;
 /**
  * 实体: -企业服务购物车
@@ -21,22 +19,8 @@ import com.gsoft.framework.core.dataobj.Domain;
 @Table(name = "sp_shoppingCar_companyServer")
 public class ShoppingcarCompanyserver implements Domain{
 	
-	private static final long serialVersionUID = 405054654136205881L;
+	private static final long serialVersionUID = -8164187421968553761L;
 	
-	/*@Id @GeneratedValue(generator="system-uuid")
-	@GenericGenerator(name="system-uuid", strategy = "uuid.hex")*/
-	@Column(name = "COMMODITY_ID_")
-	@Length(max=36)
-	private String commodityId;//商品ID
-
-	@ManyToOne(fetch=FetchType.EAGER)
-	@JoinColumn(name="MEMBER_ID_")
-	private MemberInformation memberId;//会员用户ID
-	@Id @GeneratedValue(generator="system-uuid")
-	@GenericGenerator(name="system-uuid", strategy = "uuid.hex")
-	@Column(name = "COMPANY_SERVER_ID_")
-	@Length(max=36)
-	private String companyServerId;//企业服务购物车ID
 
 	@Column(name = "COMPANY_CATERING_AMOUNT_")
 	@Length(max=16)
@@ -45,30 +29,35 @@ public class ShoppingcarCompanyserver implements Domain{
 	@Column(name = "COMPANY_CATERING_UNIVALENCE_")
 	@Length(max=16)
 	private String companyCateringUnivalence;//餐饮单价
-	
-	public String getCommodityId(){
-		return this.commodityId;
-	}
-	
-	public void setCommodityId(String commodityId){
-		this.commodityId = commodityId;
-	}
-	
-	public MemberInformation getMemberId() {
-		return memberId;
-	}
 
-	public void setMemberId(MemberInformation memberId) {
-		this.memberId = memberId;
-	}
+	@Column(name = "UPDATE_USER_")
+	@Length(max=36)
+	private String updateUser;//修改人
 
-	public String getCompanyServerId(){
-		return this.companyServerId;
-	}
+	@Column(name = "MEMBER_ID_")
+	@Length(max=36)
+	private String memberId;//会员用户ID
+
+	@Column(name = "CREATE_USER_")
+	@Length(max=36)
+	private String createUser;//创建人
+
+	@Column(name = "UPDATE_TIME_")
+	private String updateTime;//修改时间
+	@Id @GeneratedValue(generator="system-uuid")
+	@GenericGenerator(name="system-uuid", strategy = "uuid.hex")
+	@Column(name = "COMMODITY_ID_")
+	@Length(max=36)
+	private String commodityId;//商品ID
+
+	@Column(name = "CREATE_TIME_")
+	private String createTime;//创建时间
+	@Id @GeneratedValue(generator="system-uuid")
+	@GenericGenerator(name="system-uuid", strategy = "uuid.hex")
+	@Column(name = "COMPANY_SERVER_ID_")
+	@Length(max=36)
+	private String companyServerId;//企业服务购物车ID
 	
-	public void setCompanyServerId(String companyServerId){
-		this.companyServerId = companyServerId;
-	}
 	public String getCompanyCateringAmount(){
 		return this.companyCateringAmount;
 	}
@@ -83,6 +72,55 @@ public class ShoppingcarCompanyserver implements Domain{
 	public void setCompanyCateringUnivalence(String companyCateringUnivalence){
 		this.companyCateringUnivalence = companyCateringUnivalence;
 	}
+	public String getUpdateUser(){
+		return this.updateUser;
+	}
+	
+	public void setUpdateUser(String updateUser){
+		this.updateUser = updateUser;
+	}
+	public String getMemberId(){
+		return this.memberId;
+	}
+	
+	public void setMemberId(String memberId){
+		this.memberId = memberId;
+	}
+	public String getCreateUser(){
+		return this.createUser;
+	}
+	
+	public void setCreateUser(String createUser){
+		this.createUser = createUser;
+	}
+	public String getUpdateTime(){
+		return this.updateTime;
+	}
+	
+	public void setUpdateTime(String updateTime){
+		this.updateTime = updateTime;
+	}
+	public String getCommodityId(){
+		return this.commodityId;
+	}
+	
+	public void setCommodityId(String commodityId){
+		this.commodityId = commodityId;
+	}
+	public String getCreateTime(){
+		return this.createTime;
+	}
+	
+	public void setCreateTime(String createTime){
+		this.createTime = createTime;
+	}
+	public String getCompanyServerId(){
+		return this.companyServerId;
+	}
+	
+	public void setCompanyServerId(String companyServerId){
+		this.companyServerId = companyServerId;
+	}
 	
 	
 	
@@ -90,11 +128,15 @@ public class ShoppingcarCompanyserver implements Domain{
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((commodityId == null) ? 0 : commodityId.hashCode());
-		result = prime * result + ((memberId == null) ? 0 : memberId.hashCode());
-		result = prime * result + ((companyServerId == null) ? 0 : companyServerId.hashCode());
 		result = prime * result + ((companyCateringAmount == null) ? 0 : companyCateringAmount.hashCode());
 		result = prime * result + ((companyCateringUnivalence == null) ? 0 : companyCateringUnivalence.hashCode());
+		result = prime * result + ((updateUser == null) ? 0 : updateUser.hashCode());
+		result = prime * result + ((memberId == null) ? 0 : memberId.hashCode());
+		result = prime * result + ((createUser == null) ? 0 : createUser.hashCode());
+		result = prime * result + ((updateTime == null) ? 0 : updateTime.hashCode());
+		result = prime * result + ((commodityId == null) ? 0 : commodityId.hashCode());
+		result = prime * result + ((createTime == null) ? 0 : createTime.hashCode());
+		result = prime * result + ((companyServerId == null) ? 0 : companyServerId.hashCode());
 		return result;
 	}
 	
@@ -107,21 +149,6 @@ public class ShoppingcarCompanyserver implements Domain{
 		if (getClass() != obj.getClass())
 			return false;
 		final ShoppingcarCompanyserver other = (ShoppingcarCompanyserver) obj;
-		if (commodityId == null) {
-			if (other.commodityId != null)
-				return false;
-		} else if (!commodityId.equals(other.commodityId))
-			return false;
-		if (memberId == null) {
-			if (other.memberId != null)
-				return false;
-		} else if (!memberId.equals(other.memberId))
-			return false;
-		if (companyServerId == null) {
-			if (other.companyServerId != null)
-				return false;
-		} else if (!companyServerId.equals(other.companyServerId))
-			return false;
 		if (companyCateringAmount == null) {
 			if (other.companyCateringAmount != null)
 				return false;
@@ -131,6 +158,41 @@ public class ShoppingcarCompanyserver implements Domain{
 			if (other.companyCateringUnivalence != null)
 				return false;
 		} else if (!companyCateringUnivalence.equals(other.companyCateringUnivalence))
+			return false;
+		if (updateUser == null) {
+			if (other.updateUser != null)
+				return false;
+		} else if (!updateUser.equals(other.updateUser))
+			return false;
+		if (memberId == null) {
+			if (other.memberId != null)
+				return false;
+		} else if (!memberId.equals(other.memberId))
+			return false;
+		if (createUser == null) {
+			if (other.createUser != null)
+				return false;
+		} else if (!createUser.equals(other.createUser))
+			return false;
+		if (updateTime == null) {
+			if (other.updateTime != null)
+				return false;
+		} else if (!updateTime.equals(other.updateTime))
+			return false;
+		if (commodityId == null) {
+			if (other.commodityId != null)
+				return false;
+		} else if (!commodityId.equals(other.commodityId))
+			return false;
+		if (createTime == null) {
+			if (other.createTime != null)
+				return false;
+		} else if (!createTime.equals(other.createTime))
+			return false;
+		if (companyServerId == null) {
+			if (other.companyServerId != null)
+				return false;
+		} else if (!companyServerId.equals(other.companyServerId))
 			return false;
 		return true;
 	}
