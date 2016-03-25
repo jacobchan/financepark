2cb36e19d7ab3cebda6a82b4e61daed7dc7fe981
zhulizhi <zhulizhi@cd981f65-b658-4eb0-b96c-9f6a8eb296b4>
2016-03-25 04:06:38 +0000

    提交了扩展属性管理

---
+++ modules/commonCenter/commonCenter/src/main/java/com/common/ExtentionAtrManager/entity/MeetingEntity.java
@@ -0,0 +1,59 @@
+package com.common.ExtentionAtrManager.entity;
+
+/**
+ * 会议室商品扩展属性
+ * @author jack
+ *
+ */
+public class MeetingEntity {
+	
+	private String adr;//dz
+	private String gm; //gm
+	private String lx; //lx
+	private String tyy; //tyy
+	
+	public String getAdrfieldName() {
+		return "dz";
+	}
+	
+	public String getGmfieldName() {
+		return "gm";
+	}
+	
+	public String getLxfieldName() {
+		return "lx";
+	}
+	
+	public String getTyyfieldName() {
+		return "tyy";
+	}
+	
+	public String getAdr() {
+		return adr;
+	}
+	
+	public String getGm() {
+		return gm;
+	}
+	
+	public String getLx() {
+		return lx;
+	}
+	
+	public String getTyy() {
+		return tyy;
+	}
+	
+	public void setTyy(String tyy) {
+		this.tyy = tyy;
+	}
+	public void setLx(String lx) {
+		this.lx = lx;
+	}
+	public void setGm(String gm) {
+		this.gm = gm;
+	}
+	public void setAdr(String adr) {
+		this.adr = adr;
+	}
+}
