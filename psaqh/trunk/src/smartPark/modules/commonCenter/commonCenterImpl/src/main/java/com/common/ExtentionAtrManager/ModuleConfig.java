3e5052ea2593c46c5c407f04c4165dbb46ceb8d8
zhulizhi <zhulizhi@cd981f65-b658-4eb0-b96c-9f6a8eb296b4>
2016-03-25 04:07:32 +0000

    提交了扩展属性管理

---
+++ psaqh/trunk/src/smartPark/modules/commonCenter/commonCenterImpl/src/main/java/com/common/ExtentionAtrManager/ModuleConfig.java
@@ -0,0 +1,20 @@
+package com.common.ExtentionAtrManager;
+
+
+import com.gsoft.framework.context.annotation.Module;
+
+/**
+ * <p>@系统描述:YOUI</p>
+ * <p>@功能描述:</p>
+ * <p>@作者：  Administrator</p>
+ * <p>@版本 ：1.0.0</p>
+ * <p>@创建时间： 上午11:48:47</p>
+ */
+
+@Module(
+	name="ExtentionAtrManager",
+	caption = "ExtentionAtrManager"
+)
+public class ModuleConfig {
+	
+}
