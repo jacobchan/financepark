9817484a35bde3dfbc57bf1c4f0411610b29d7f5
linwz <linwz@cd981f65-b658-4eb0-b96c-9f6a8eb296b4>
2016-04-19 02:11:56 +0000

    酒店机票http请求

--- psaqh/trunk/src/smartPark/modules/common/src/main/java/com/gsoft/utils/HttpGetAndPostUtil.java
+++ psaqh/trunk/src/smartPark/modules/common/src/main/java/com/gsoft/utils/HttpGetAndPostUtil.java
@@ -13,7 +13,7 @@ import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.Map;
 
-public class HotelUtil {
+public class HttpGetAndPostUtil {
 	public static final String DEF_CHATSET = "UTF-8";
 	public static final int DEF_CONN_TIMEOUT = 30000;
 	public static final int DEF_READ_TIMEOUT = 30000;
@@ -118,7 +118,7 @@ public class HotelUtil {
 		return rs;
 	}
 	//POST http请求
-	public static String sendPost(String strUrl, Map<String, Object> params) throws Exception {
+	public static String HotelSendPost(String strUrl, Map<String, Object> params) throws Exception {
 		PrintWriter out = null;
 		BufferedReader in = null;
 		String result = "";
@@ -142,6 +142,45 @@ public class HotelUtil {
 			out.flush();
 			// 定义BufferedReader输入流来读取URL的响应
 			in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += line;
+			}
+			return result;
+		} catch (Exception e) {
+			System.out.println("发送 POST 请求出现异常：" + e);
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	//POST http请求
+	public static String TicketSendPost(String strUrl, Map<String, Object> params) throws Exception {
+		PrintWriter out = null;
+		BufferedReader in = null;
+		String result = "";
+		try {
+			URL realUrl = new URL(strUrl);
+			// 打开和URL之间的连接
+			URLConnection conn = realUrl.openConnection();
+			// 设置通用的请求属性
+			conn.setRequestProperty("Charset", "UTF-8");
+			conn.setRequestProperty("accept", "*/*");
+			conn.setRequestProperty("connection", "Keep-Alive");
+			conn.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");		
+			// 发送POST请求必须设置如下两行
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			// 获取URLConnection对象对应的输出流
+			out = new PrintWriter(conn.getOutputStream());
+			// 发送请求参数
+			out.print(urlencode(params));
+			// flush输出流的缓冲
+			out.flush();
+			// 定义BufferedReader输入流来读取URL的响应
+			//in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			in = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
 			String line;
 			while ((line = in.readLine()) != null) {
 				result += line;
