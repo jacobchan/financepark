444f861751c6cc898e1158d8bf813cb7d34806ad
zhulizhi <zhulizhi@cd981f65-b658-4eb0-b96c-9f6a8eb296b4>
2016-04-26 04:05:06 +0000

    添加评论星数

--- psaqh/trunk/src/smartPark/modules/manageCenter/manageCenter/src/main/java/com/manage/ActivityManager/entity/ActivityDocument.java
+++ psaqh/trunk/src/smartPark/modules/manageCenter/manageCenter/src/main/java/com/manage/ActivityManager/entity/ActivityDocument.java
@@ -49,6 +49,17 @@ public class ActivityDocument implements Domain{
 	@Column(name = "CREATE_TIME_")
 	private String createTime;//创建时间
 	
+	@Column(name = "COMMENT_LEVEL_")
+	private String commentLevel;//星数
+	
+	public String getCommentLevel() {
+		return commentLevel;
+	}
+
+	public void setCommentLevel(String commentLevel) {
+		this.commentLevel = commentLevel;
+	}
+
 	@ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name="APPLY_ID_")
 	private com.manage.ActivityManager.entity.ActivityApply activityApply;//活动申请ID
@@ -123,6 +134,7 @@ public class ActivityDocument implements Domain{
 		result = prime * result + ((documentPath == null) ? 0 : documentPath.hashCode());
 		result = prime * result + ((createUser == null) ? 0 : createUser.hashCode());
 		result = prime * result + ((createTime == null) ? 0 : createTime.hashCode());
+		result = prime * result + ((commentLevel == null) ? 0 : commentLevel.hashCode());
 		return result;
 	}
 	
@@ -169,6 +181,11 @@ public class ActivityDocument implements Domain{
 			if (other.createTime != null)
 				return false;
 		} else if (!createTime.equals(other.createTime))
+			return false;
+		if (commentLevel == null) {
+			if (other.commentLevel != null)
+				return false;
+		} else if (!commentLevel.equals(other.commentLevel))
 			return false;
 		return true;
 	}
