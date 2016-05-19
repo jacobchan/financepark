553fe556595515256479ecfb6fa431c47c608304
linwz <linwz@cd981f65-b658-4eb0-b96c-9f6a8eb296b4>
2016-05-19 11:20:07 +0000

    酒店机票接口

--- modules/memberCenter/memberCenterImpl/src/main/java/com/member/ticket/dao/hibernate/RefundOrderDaoHibernate.java
+++ modules/memberCenter/memberCenterImpl/src/main/java/com/member/ticket/dao/hibernate/RefundOrderDaoHibernate.java
@@ -3,10 +3,13 @@
  */
 package com.member.ticket.dao.hibernate;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 import com.gsoft.framework.core.dao.hibernate.BaseDaoHibernate;
 
 import com.member.ticket.entity.RefundOrder;
+import com.member.ticket.entity.TicketPassenger;
 import com.member.ticket.dao.RefundOrderDao;
 
 @Repository("RefundOrderDao")
@@ -14,5 +17,23 @@ public class RefundOrderDaoHibernate extends
 		BaseDaoHibernate<RefundOrder, String> implements RefundOrderDao{
 	public Class<RefundOrder> getModelClazz(){
 		return RefundOrder.class;
+	}
+
+	@Override
+	public RefundOrder findByRefundNo(String sequenceNo) {
+		String hql = "from sp_refund_order m where "
+				+ "m.REFUND_ORDER_NO_ = " + sequenceNo;
+		List<RefundOrder> list = getHibernateTemplate().find(hql);
+		if (!list.isEmpty()) {
+			return list.get(0);
+		}else{
+			return null;
+		}
+	}
+
+	@Override
+	public void update(RefundOrder refundOrder) {
+		getHibernateTemplate().update(refundOrder);
+		getHibernateTemplate().flush();
 	}
 }
--- modules/memberCenter/memberCenterImpl/src/main/java/com/member/ticket/dao/hibernate/TicketOrderDaoHibernate.java
+++ modules/memberCenter/memberCenterImpl/src/main/java/com/member/ticket/dao/hibernate/TicketOrderDaoHibernate.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import com.gsoft.framework.core.dao.hibernate.BaseDaoHibernate;
 
 import com.member.ticket.entity.TicketOrder;
+import com.member.ticket.entity.TicketPassengerRelation;
 import com.member.ticket.dao.TicketOrderDao;
 
 @Repository("TicketOrderDao")
@@ -14,5 +15,23 @@ public class TicketOrderDaoHibernate extends
 		BaseDaoHibernate<TicketOrder, String> implements TicketOrderDao{
 	public Class<TicketOrder> getModelClazz(){
 		return TicketOrder.class;
+	}
+
+	@Override
+	public TicketOrder findByOrderItem(String itemId) {
+		String hql = "from sp_ticket_order m where "
+				+ "m.ITEM_ID_ = " + itemId;
+		if (!getHibernateTemplate().find(hql).isEmpty()) {
+			return (TicketOrder) getHibernateTemplate().find(hql).get(0);
+		}else{
+			return null;
+		}
+		
+	}
+
+	@Override
+	public void update(TicketOrder ticketOrder) {
+		getHibernateTemplate().update(ticketOrder);
+		getHibernateTemplate().flush();
 	}
 }
