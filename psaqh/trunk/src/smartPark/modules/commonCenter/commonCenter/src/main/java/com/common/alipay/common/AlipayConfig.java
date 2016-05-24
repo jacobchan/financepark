f4bd43f31cc147c37806fc051566bbab4cb1ff0e
tanyaxiong <tanyaxiong@cd981f65-b658-4eb0-b96c-9f6a8eb296b4>
2016-05-24 08:40:15 +0000

    支付宝签名生成

---
+++ psaqh/trunk/src/smartPark/modules/commonCenter/commonCenter/src/main/java/com/common/alipay/common/AlipayConfig.java
@@ -0,0 +1,50 @@
+package com.common.alipay.common;
+
+public class AlipayConfig {
+	
+	//↓↓↓↓↓↓↓↓↓↓请在这里配置您的基本信息↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+
+		// 合作身份者ID，签约账号，以2088开头由16位纯数字组成的字符串，查看地址：https://b.alipay.com/order/pidAndKey.htm
+		public static String partner = "2088221352269831";
+		
+		// 收款支付宝账号，以2088开头由16位纯数字组成的字符串，一般情况下收款账号就是签约账号
+		public static String seller_id = partner;
+
+		// MD5密钥，安全检验码，由数字和字母组成的32位字符串，查看地址：https://b.alipay.com/order/pidAndKey.htm
+	    public static String key = "p11xth0ku16gh4wllj1s2m4ikqlcb1jy";
+
+		// 服务器异步通知页面路径  需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
+		public static String notify_url = "http://www.qypark.com/manage/wxpay/notify.json";
+
+		// 页面跳转同步通知页面路径 需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
+		public static String return_url = "http://www.qypark.com/manage/wxpay/notify.json";
+
+		// 签名方式
+		public static String sign_type = "MD5";
+		
+		// 调试用，创建TXT日志文件夹路径，见AlipayCore.java类中的logResult(String sWord)打印方法。
+		public static String log_path = "C:\\";
+			
+		// 字符编码格式 目前支持 gbk 或 utf-8
+		public static String input_charset = "utf-8";
+			
+		// 支付类型 ，无需修改
+		public static String payment_type = "1";
+			
+		// 调用的接口名，无需修改
+		public static String service = "create_direct_pay_by_user";
+
+
+	//↑↑↑↑↑↑↑↑↑↑请在这里配置您的基本信息↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+		
+	//↓↓↓↓↓↓↓↓↓↓ 请在这里配置防钓鱼信息，如果没开通防钓鱼功能，为空即可 ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+		
+		// 防钓鱼时间戳  若要使用请调用类文件submit中的query_timestamp函数
+		public static String anti_phishing_key = "";
+		
+		// 客户端的IP地址 非局域网的外网IP地址，如：221.0.0.1
+		public static String exter_invoke_ip = "127.0.0.1";
+			
+	//↑↑↑↑↑↑↑↑↑↑请在这里配置防钓鱼信息，如果没开通防钓鱼功能，为空即可 ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+		
+}
